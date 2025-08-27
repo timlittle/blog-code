379b93addb38c711377209fef1c7ee7a0569901e
Tim <timlittle88@gmail.com>
2025-08-27 21:13:12 +0100

    Add the initial game state and draw

--- go-gameoflife/main.go
+++ go-gameoflife/main.go
@@ -3,13 +3,48 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 type Game struct {
-	Height int
-	Width  int
+	Height   int
+	Width    int
+	tileSize int
+	State    [][]int
+}
+
+func (g *Game) Draw() {
+	// Loop through all of the rows
+	for y := range g.State {
+
+		// Loop through all of the columes
+		for x := 0; x < len(g.State[y]); x++ {
+
+			// If we have marked the column as a 1, draw it as white
+			if g.State[y][x] == 1 {
+
+				// We will need to scale our blocks to the size of the window
+				pixelX := x * g.tileSize
+				pixelY := y * g.tileSize
+
+				// Draw the block to the screen
+				rl.DrawRectangle(int32(pixelX), int32(pixelY), int32(g.tileSize), int32(g.tileSize), rl.RayWhite)
+			}
+		}
+	}
+}
+
+func NewGame(width, height, tileSize int) *Game {
+	g := &Game{Width: width, Height: height, tileSize: tileSize}
+	g.State = [][]int{
+		{0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+		{1, 1, 1, 0, 0, 0, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	return g
 }
 
 func main() {
 	// Create the game metadata and state holding object
-	game := Game{Width: 800, Height: 400}
+	var game = NewGame(800, 400, 80)
 
 	// Create the Raylib window using the state
 	rl.InitWindow(int32(game.Width), int32(game.Height), "Game of life")
@@ -28,8 +63,8 @@ func main() {
 		// Create a black background
 		rl.ClearBackground(rl.Black)
 
-		// Draw Hello world
-		rl.DrawText("Hello world!", 350, 200, 20, rl.RayWhite)
+		// Draw the game state
+		game.Draw()
 
 		// End the drawing
 		rl.EndDrawing()
