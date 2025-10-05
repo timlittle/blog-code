801ebfc08e5eeb7468be3f3ee9e2d680277f6b29
Tim <timlittle88@gmail.com>
2025-10-05 21:18:01 +0100

    Draw the player

--- go-asteroids/main.go
+++ go-asteroids/main.go
@@ -7,7 +7,47 @@ import (
 const (
 	screenWidth  = 800
 	screenHeight = 400
+	tileSize     = 64
 )
+
+var (
+	texTiles      rl.Texture2D
+	texBackground rl.Texture2D
+	spriteRec     rl.Rectangle
+	player        Player
+)
+
+type Player struct {
+	position     rl.Vector2
+	speed        rl.Vector2
+	size         rl.Vector2
+	acceleration float32
+	rotation     float32
+	isBoosting   bool
+}
+
+func (p *Player) Draw() {
+	destTexture := rl.Rectangle{X: p.position.X, Y: p.position.Y, Width: p.size.X, Height: p.size.Y}
+	rl.DrawTexturePro(
+		texTiles,
+		spriteRec,
+		destTexture,
+		rl.Vector2{X: p.size.X / 2, Y: p.size.Y / 2},
+		p.rotation,
+		rl.White,
+	)
+}
+
+func initGame() {
+	player = Player{
+		position:     rl.Vector2{X: 400, Y: 200},
+		speed:        rl.Vector2{X: 0.0, Y: 0.0},
+		size:         rl.Vector2{X: tileSize, Y: tileSize},
+		rotation:     0.0,
+		acceleration: 0.0,
+		isBoosting:   false,
+	}
+}
 
 func init() {
 	//Built go function which runs before main()
@@ -17,12 +57,13 @@ func init() {
 	rl.SetTargetFPS(60)
 
 	// Load textures
+	texTiles = rl.LoadTexture("resources/tilesheet.png")
 	texBackground = rl.LoadTexture("resources/space_background.png")
-}
 
-var (
-	texBackground rl.Texture2D
-)
+	spriteRec = rl.Rectangle{X: tileSize * 0, Y: tileSize * 2, Width: tileSize, Height: tileSize}
+
+	initGame()
+}
 
 func draw() {
 	rl.BeginDrawing()
@@ -31,6 +72,9 @@ func draw() {
 	bgSource := rl.Rectangle{X: 0, Y: 0, Width: float32(texBackground.Width), Height: float32(texBackground.Height)}
 	bgDest := rl.Rectangle{X: 0, Y: 0, Width: screenWidth, Height: screenHeight}
 	rl.DrawTexturePro(texBackground, bgSource, bgDest, rl.Vector2{X: 0, Y: 0}, 0, rl.White)
+
+	//Draw the player
+	player.Draw()
 
 	// Draw the score to the screen
 	rl.DrawText("Score 0", 10, 10, 20, rl.Gray)
@@ -49,6 +93,7 @@ func deinit() {
 
 	// Unload textures when the game closes
 	rl.UnloadTexture(texBackground)
+	rl.UnloadTexture(texTiles)
 }
 
 func main() {
