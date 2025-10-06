59302777c639423b55d07bc68fb465bb8128d69a
Tim <timlittle88@gmail.com>
2025-10-06 20:10:48 +0100

    Add moving asteroids

--- go-asteroids/main.go
+++ go-asteroids/main.go
@@ -7,11 +7,12 @@ import (
 )
 
 const (
-	screenWidth   = 800
-	screenHeight  = 400
-	tileSize      = 64
-	rotationSpeed = 2.0
-	playerSpeed   = 6.0
+	screenWidth      = 800
+	screenHeight     = 400
+	tileSize         = 64
+	rotationSpeed    = 2.0
+	playerSpeed      = 6.0
+	initialAsteroids = 5
 )
 
 var (
@@ -19,6 +20,8 @@ var (
 	texBackground rl.Texture2D
 	spriteRec     rl.Rectangle
 	boostRec      rl.Rectangle
+	asteroidRec   rl.Rectangle
+	asteroids     []Asteroid
 	player        Player
 )
 
@@ -97,6 +100,98 @@ func (p *Player) Update() {
 	wrapPosition(&p.position, tileSize)
 }
 
+// Enum for storing the size of the asteroid
+type AsteroidSize int
+
+const (
+	Large AsteroidSize = iota
+	Medium
+	Small
+)
+
+type Asteroid struct {
+	position     rl.Vector2
+	speed        rl.Vector2
+	size         rl.Vector2
+	asteroidSize AsteroidSize
+}
+
+func (a *Asteroid) Draw() {
+	// Draw the asteroid to the screen
+	destTexture := rl.Rectangle{X: a.position.X, Y: a.position.Y, Width: a.size.X, Height: a.size.Y}
+	rl.DrawTexturePro(
+		texTiles,
+		asteroidRec,
+		destTexture,
+		rl.Vector2{X: a.size.X / 2, Y: a.size.Y / 2},
+		0.0,
+		rl.White,
+	)
+}
+
+func (a *Asteroid) Update() {
+	// Move the asteroid in its direction
+	a.position = rl.Vector2Add(a.position, a.speed)
+
+	// Wrap the position, so they are always on screen
+	wrapPosition(&a.position, a.size.X)
+}
+
+// Asteroid helper functions
+func createLargeAsteroid() Asteroid {
+
+	// Generate a random edge of the screen to spawn
+	randomEdge := rl.GetRandomValue(0, 3)
+	var position rl.Vector2
+
+	// Generate a random position on screen
+	randomX := float32(rl.GetRandomValue(0, screenWidth))
+	randomY := float32(rl.GetRandomValue(0, screenHeight))
+
+	switch randomEdge {
+	case 0:
+		position = rl.Vector2{X: randomX, Y: +tileSize}
+	case 1:
+		position = rl.Vector2{X: screenWidth + tileSize, Y: randomY}
+	case 2:
+		position = rl.Vector2{X: randomX, Y: screenHeight + tileSize}
+	case 3:
+		position = rl.Vector2{X: -tileSize, Y: randomY}
+	}
+
+	// Generate a random speed and direction for the asteroid
+	speed := rl.Vector2{
+		X: float32(rl.GetRandomValue(-10, 10)) / 10,
+		Y: float32(rl.GetRandomValue(-10, 10)) / 10,
+	}
+
+	// Create the large asteroid
+	return createAsteroid(Large, position, speed)
+}
+
+func createAsteroid(asteroidSize AsteroidSize, position, speed rl.Vector2) Asteroid {
+
+	// Scale the image of the asteroid based on the asteroidSize
+	var size rl.Vector2
+	switch asteroidSize {
+	case Large:
+		size = rl.Vector2{X: tileSize * 1.0, Y: tileSize * 1.0}
+	case Medium:
+		size = rl.Vector2{X: tileSize * 0.7, Y: tileSize * 0.7}
+	case Small:
+		size = rl.Vector2{X: tileSize * 0.4, Y: tileSize * 0.4}
+	}
+
+	// Create the asteroid
+	return Asteroid{
+		position:     position,
+		speed:        speed,
+		size:         size,
+		asteroidSize: asteroidSize,
+	}
+}
+
+// Helper functions
 func getDirectionVector(rotation float32) rl.Vector2 {
 	// Convert the rotation to radians
 	radians := float64(rotation) * rl.Deg2rad
@@ -128,6 +223,13 @@ func wrapPosition(pos *rl.Vector2, objectSize float32) {
 }
 
 func initGame() {
+	// Create the asteroids field
+	asteroids = nil
+	for range initialAsteroids {
+		asteroids = append(asteroids, createLargeAsteroid())
+	}
+
+	// Create the player
 	player = Player{
 		position:     rl.Vector2{X: 400, Y: 200},
 		speed:        rl.Vector2{X: 0.0, Y: 0.0},
@@ -153,6 +255,9 @@ func init() {
 	spriteRec = rl.Rectangle{X: tileSize * 0, Y: tileSize * 2, Width: tileSize, Height: tileSize}
 	boostRec = rl.Rectangle{X: tileSize * 7, Y: tileSize * 5, Width: tileSize, Height: tileSize}
 
+	// Sprite for the asteroid
+	asteroidRec = rl.Rectangle{X: tileSize * 1, Y: tileSize * 4, Width: tileSize, Height: tileSize}
+
 	initGame()
 }
 
@@ -167,6 +272,11 @@ func draw() {
 	//Draw the player
 	player.Draw()
 
+	// Draw the asteroid field
+	for i := range asteroids {
+		asteroids[i].Draw()
+	}
+
 	// Draw the score to the screen
 	rl.DrawText("Score 0", 10, 10, 20, rl.Gray)
 
@@ -175,7 +285,14 @@ func draw() {
 }
 
 func update() {
+	// Update the player
 	player.Update()
+
+	// Update the asteroid field
+	for i := range asteroids {
+		asteroids[i].Update()
+	}
+
 }
 
 func deinit() {
